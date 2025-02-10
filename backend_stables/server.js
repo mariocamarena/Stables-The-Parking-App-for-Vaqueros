fd3a7c6eee8d7ad961bf927fd5146ec3077755bd
dabioly <mario-camarena12@hotmail.com>
2025-02-10 14:01:09 -0600

    basic server + dummy data

--- backend_stables/server.js
+++ backend_stables/server.js
@@ -9,5 +9,16 @@ app.get('/', (req, res) => {
 });
 
 //start server
+
+app.get('/parking',(req,res) => {
+  res.json({
+    lot_id: 'LOT_E16',
+    zone_type: 'zone_2',
+    total_spots: 5,
+    available_spots: 2,
+    updated_at: new Date().toISOString()
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
